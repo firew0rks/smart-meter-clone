aabd45dbba7869a6074b7569c127b23bdc1d5eb5
Wu, Jessica A <jessica.a.wu@accenture.com>
2018-02-20 08:30:19 +1100

    scaffold for power contract

--- contracts/Power.sol
+++ contracts/Power.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.4.18;
 
 function Power {
-  
+  // Define variables
+    // list of people taking part
+    // struct for each person containing their address, how much money they have left, how much money they are generating, history of transactions
+    // Surge charge
+
+
+  // Function: amount of tokens left
+
+
+  // Function: escrow - once receipt is received for token payment, transfer energy
+
+  // Function: push transfer of tokens from consumer to producer
+
+
+  // Function: record energy transfer from producer to consumer
+
+  // Function: view historical data
+
+
+  // Function: automate the report of stats after 3 years
+
+
+
 }
